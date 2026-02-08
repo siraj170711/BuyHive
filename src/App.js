c3c010e135ad7eda5dc729d33ba21214d574473a
siraj170711 <k.mohamedsirajudeent8@gmail.com>
2026-02-08 11:42:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
           </div>
         </div>
       </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
       <Routes>
         <Route path="/" element={<Home />} />
